25bc6affbabc9820088bc5a3bddeb22d5814f2b0
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-01-15 09:08:04 +0300

    adding the navigation drawer

--- lib/my_onboard_page.dart
+++ lib/my_onboard_page.dart
@@ -9,6 +9,30 @@ class MyOnboardPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("restaurant"),
       ),
+      drawer: Drawer(
+        // backgroundColor: Colors.deepPurpleAccent,
+        child: Column(
+          children: [
+            const DrawerHeader(
+              child: Icon(Icons.ac_unit_outlined),
+            ),
+            ListTile(
+              leading: const Icon(Icons.import_contacts_rounded),
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.air),
+              title: const Text("page 2"),
+              onTap: () {
+                Navigator.pushNamed(context, '/pagetwo');
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
